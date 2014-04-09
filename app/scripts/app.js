a58e426971a56905dc673cc0610e3fa5fd076cd2
Victor Pascual <victor.pascual@gmail.com>
2014-04-09 15:09:35 +0200

    Initial yeoman scafolding

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular
+  .module('businessSchoolsApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
