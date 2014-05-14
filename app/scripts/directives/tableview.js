aa4c174afd1769d1e7a05faa96b5a1908841847d
Victor Pascual <victor.pascual@gmail.com>
2014-05-14 15:56:38 +0200

    Developed fixed header

--- app/scripts/directives/tableview.js
+++ app/scripts/directives/tableview.js
@@ -21,24 +21,36 @@ angular.module('businessSchoolsApp')
           // data = [data[0], data[1], data[2]]          
         })
 
-        d3.select("#tablediv")
-          .attr("height", 300 + "px")
+        var width = scope.width - scope.margin.left - scope.margin.right - 57;
+        d3.select("#tablewrapper")
+          // .attr("height", 300 + "px")
           // .style("width", scope.width - scope.margin.left - scope.margin.right  + "px");
-          .style("width", scope.width - scope.margin.left - scope.margin.right - 57 + "px")
+          .style("width",  width + "px")
           .style("margin-left", scope.margin.left + 28 + "px"); 
 
         d3.selectAll("thead tr").selectAll("th")
             .data(['Name', 'Country'].concat(scope.dimensions))
             .enter()
             .append("th")
-            // .attr("class", function(d, i) {
-            //   if (d != "Name")
-            //     return "centeredTd";
-            //   else
-            //     return null;
-            // })         
-            .style("background-color", "#eeeeee")
+              .style("width", function(d, i) {
+                // console.log("Setting style " + i)
+                if (i == 0)
+                  return "290px"
+                else 
+                  if (i == 1)
+                    return "145px"
+
+                return "30px"
+                // return (width - 300 - 150) / scope.dimensions.length;
+                // return (scope.width - scope.margin.left - scope.margin.right - 57)/17 + "px"
+              })
+              .style("border", "1")
+              .style("text-align", "center")
+              .style("background-color", "#eeeeee")
               .text(function(d) { return d; })
+
+        d3.select("#tablewrapper table")
+              .style("width", width - 15 + "px")
 
         var color = d3.scale.linear()
             .domain([101, 1])
@@ -54,14 +66,20 @@ angular.module('businessSchoolsApp')
               .on("mouseover", function(d) { 
                 scope.highlightParallel(d); 
                 d3.select(this)
-                  .select("td")
-                    .style("font-weight", "bold")
+                  // .select("td")
+                  //   .style("font-weight", "bold")
+                  // .selectAll("td")
+                    // .style("border", "1px solid black")
+                    .style('outline', 'thin solid #888')
               })
               .on("mouseout", function(d) { 
                 scope.unHighlightParallel(); 
                 d3.select(this)
-                  .select("td")
-                    .style("font-weight", "normal")
+                  // .select("td")
+                  //   .style("font-weight", "normal")
+                  // .selectAll("td")
+                  //   .style("border", null)
+                  .style('outline', null)
               });
             // .style("background-color", function(d,i) {
             //   return (i%2 == 0) ? "white" : "#F0F0F0";
@@ -81,21 +99,40 @@ angular.module('businessSchoolsApp')
               
               return array; 
             })
-            .enter()
-            .append("td")
+            .enter()            
+            .append("td")                                                   
               .attr("class", function(d, i) {
                 return (i > 1) ? "centeredTd" : null;
               })
-              .style("font", function(d, i) {
-                if (i == scope.dimensions.length + 1)
-                  return "italic bold";
-              })
+              // .style("font", function(d, i) {
+              //   if (i == scope.dimensions.length)
+              //     return "italic bold";
+              // })
               .style("background-color", function(d, i) {  
                 return (d == 0) ? color(101) : color(d); 
               })
+              .style("width", function(d, i) {
+                // console.log("Setting style " + i)
+                if (i == 0)
+                  return "300px"
+                else 
+                  if (i == 1)
+                    return "150px"
+
+                return "30px"
+                // return (width - 300 - 150) / scope.dimensions.length;
+                // return (scope.width - scope.margin.left - scope.margin.right - 57)/17 + "px"
+              })
+              .style("text-align", function(d, i) {
+                if (i < 2)
+                  return null;
+                
+                return "center";
+              })
               .text(function(d, i) {
                 return (d == 0) ? "-" : d;
-              });
+              })
+
         }
       }
     };
